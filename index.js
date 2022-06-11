97fe56240d43ac00e43791c897a12ea500d4e0e8
Noibi Abdulrasaq <noibiabdulrasaq@gmail.com>
2022-06-11 03:59:21 +0100

    Creating a repository and saving all files

---
+++ index.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const replaceTemplate = require('./modules.js/replaceTemplate');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// FILES
+// Synchronous Blocking Way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
+
+// Non-blocking, Asynchronous Non-Blocking Way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR! 💥');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written!🥰');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read this ⬇️');
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SERVER
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+
+const templCards = fs.readFileSync(
+  `${__dirname}/templates/template-cards.html`,
+  'utf-8'
+);
+const dataObj = JSON.parse(data);
+console.log(dataObj);
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url);
+  console.log(url.parse(req.url, true));
+  // const path = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  console.log(query);
+
+  // OVERVIEW PAGE
+  if (pathname === '/overview' || pathname === '/') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHTML = dataObj
+      .map((el) => replaceTemplate(templCards, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+    res.end(output);
+    // console.log(cardsHTML);
+
+    // PRODUCT PAGE
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+    // API PAGE
+  } else if (pathname === '/api') {
+    res.end(data);
+    res.writeHead(200, { 'Content-type': 'application/json' });
+
+    // NOT FOUND PAGE
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end(`<h1>Page not found!</h1>`);
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening on port 8000');
+});
